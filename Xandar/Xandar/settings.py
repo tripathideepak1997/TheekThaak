617c5c6d33e90a3ca5f08f949f5bbf8ca68564ee
Aayush Gupta <aayushgupta2097@gmail.com>
2019-05-16 14:36:11 +0530

    implementing password reset completed without templates

--- Xandar/Xandar/settings.py
+++ Xandar/Xandar/settings.py
@@ -125,3 +125,14 @@ STATICFILES_DIRS = (
 )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/'
+
+
+
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'contact.realestate.information@gmail.com'
+EMAIL_HOST_PASSWORD = 'realestate1234'
+EMAIL_USE_TLS = True
--- Xandar/Xandar/urls.py
+++ Xandar/Xandar/urls.py
@@ -17,10 +17,36 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from Xandar import settings
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
+
+
+
+    path('password-reset', 
+    	auth_views.PasswordResetView.as_view(
+    		template_name='accounts/password_reset.html'),
+    		 name="password_reset"),
+
+    path('password-reset/done', 
+    	auth_views.PasswordResetDoneView.as_view(
+    		template_name='accounts/password_reset_done.html'),
+    		 name="password_reset_done"),
+
+    path('password-reset-complete/', 
+    	auth_views.PasswordResetCompleteView.as_view(
+    		template_name='accounts/password_reset_complete.html'),
+    		 name="password_reset_complete"),
+
+    path('password-reset-confirm/<uidb64>/<token>/', 
+    	auth_views.PasswordResetConfirmView.as_view(
+    		template_name='accounts/password_reset_confirm.html'),
+    		 name="password_reset_confirm"),
+
+
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
