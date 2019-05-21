9d392c3ec690c8e722920fa3715c94ad3c23f81c
Aman <amanjai01.gmail.com>
2019-05-21 11:05:33 +0530

    Models now added

--- Xandar/core/admin.py
+++ Xandar/core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from core.models import Customer
+#from core.models import Customer, Attribute, Product, ExtraAttribute, ProductImages
+from core.models import *
 
 # Register your models here.
-admin.site.register(Customer)
+#admin.site.register(Customer)
+#admin.site.register(Product)
+#admin.site.register(ProductImages)
+#admin.site.register(Attribute)
+#admin.site.register(ExtraAttribute)
